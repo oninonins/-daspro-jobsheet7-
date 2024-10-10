2b0a7c8ba2b506b654dd5c60e7350b281fe4e383
oninonins <varelinobubadibako11@gmail.com>
2024-10-10 15:12:42 +0700

    revisi lulus

--- SiakadFor05.java
+++ SiakadFor05.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
-
-
 public class SiakadFor05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        int jmlLulus = 0, jmlTdkLulus = 0, batasLulus = 60 ;
         double nilai, tertinggi = 0, terendah = 100;
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Masukan nilai mahasiswa ke-" + i + ":");
+            System.out.print("Masukan nilai mahasiswa ke-" + i + ":");
             nilai = input.nextDouble();
             if (nilai > tertinggi) {
                 tertinggi = nilai ;
@@ -15,11 +13,20 @@ public class SiakadFor05 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai <= batasLulus) {
+                jmlLulus++;
+            }else {
+                jmlTdkLulus++;
+            }
+
 
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah lulus: " + jmlLulus);
+        System.out.println("Jumlah tidak lulus: " + jmlTdkLulus);
+
     }
     
 }
